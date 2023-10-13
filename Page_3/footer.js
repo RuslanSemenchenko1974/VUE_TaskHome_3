01a9d7e9acab50cf38e629f539b46e3ec76e8f6b
Ruslan <89604587433@mail.ru>
2023-10-13 13:28:43 +0300

    HomeTask 3

---
+++ Page_3/footer.js
@@ -0,0 +1,27 @@
+const footer_sight = {
+    template: `    
+    <footer class="footer">
+  
+    <div class="footer__logo">
+        <div class="header__left"> <img class="logo" src="image/Logo.svg" alt="logo"> Interno </div>
+        <p class="footer__logo__text">
+            It is a long established fact that a reader <br> will be distracted lookings.
+        </p>
+        <img class="footer__logo__img" src="image/Social.svg" alt="">
+    </div>
+
+    <div class="footer__pages">
+        <h1 class="footer__pages__title">Pages</h1>
+        <a href="" class="footer__pages__reference">Home</a>
+        <a href="" class="footer__pages__reference">Project</a>
+        <a href="" class="footer__pages__reference">Blog</a>
+    </div>
+
+    <div class="footer__contact">
+        <h2 class="footer__pages__title">Contact</h2>
+        <p class="footer__contact__text">55 East Birchwood Ave. Brooklyn, New York 11201 <br> <br> contact@interno.com <br><br>
+            (123) 456 - 7890</p>
+    </div>
+</footer>
+     `,
+ }
---
+++ Page_3/header.js
@@ -0,0 +1,14 @@
+const header_sight = {
+   template: `    
+    <header class="header">
+      <div class="header__left">
+        <img class="logo" src="image/Logo.svg" alt="logo" /> Interno
+      </div>
+      <div class="header__right">
+        <a href="#" class="header__refer">Home</a>
+        <a href="#" class="header__refer">Project</a>
+        <a href="#" class="header__refer">Blog</a>
+      </div>
+    </header>
+    `,
+}
